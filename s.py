d10ed532f2eba62b78ae682c12d4b1403e7a7192
YLTsai0609 <yltsai0609@gmail.com>
2020-11-11 23:08:10 +0800

    [refactor] 將plot_feature_corr_sorting拆分為3個函數

--- s.py
+++ s.py
@@ -2,6 +2,14 @@
 特徵與目標的相關性分析，以house_price為例
 將各個特徵與目標的相關係數畫出，排序後，並以bar plot表示
 希望以後可以重複利用這些函數
+
+-----------------------------------
+SRP 單一職責原則，將plot_feature_corr_sorting這隻函數拆小
+並以時常更動地方進行拆分
+1. 要計算相關係數的特徵欄位可能會經常過濾 - filter_col
+2. 畫圖可能會經常修改畫圖參數，例如xy座標，標題等 - plot_barh
+plot_feature_corr_sorting 修改為calculate_correlation
+
 '''
 
 import pandas as pd
@@ -39,31 +47,71 @@ def colormap2rgb(num_colors: int,
     return rgba_color, rgb_color
 
 
-# +
-def plot_feature_corr_sorting(df: pd.DataFrame, target: str,
-                              verbose: bool = False,
-                              fig_title: str = 'Correlations with Target') -> None:
+def filter_col(df: pd.DataFrame, dtype: str = 'number') -> pd.DataFrame:
     """
-    輸入dataframe，畫出對各個數值特徵對目標的相關係數
+    從整個dataframe中過濾出特定欄位
+    本函數會選出欄位資料型態不是object的欄位
+
+    Args : 
+        df (pd.DataFrame): 整個需要分析的dataframe
+    Returns : 
+        df (pd.DataFrame): 經過過濾的dataframe
+    """
+    if dtype == 'number':
+        cols = [var for var in df.columns if df[var].dtypes != 'O']
+    else:
+        raise NotImplementedError(f'尚未實作該過濾機制，你目前輸入的dype為 {dtype}')
+    return df[cols]
+
+
+def plot_barh(df: pd.DataFrame,
+              y='corr_with_targets',
+              x='features',
+              fig_title='Correlations with Target') -> None:
+    """
+    畫出Barh
 
     Args:
-        df (pd.DataFrame): 整個需要分析的dataframe
-        target (str): 目標變數
-        verbose(bool) : 是否需要顯示各個變數對目標變數的相關係數 Default to False
+        df (pd.DataFrame): 要畫的圖
+        y (str): y軸座標字串
+        x (str): x軸座標字串
         fig_title(str) : 畫圖標題
 
     Returns:
         None
 
     """
+    rgba_list, _ = colormap2rgb(num_colors=df.shape[0])
+    with plt.style.context(['science', 'grid', 'no-latex']):
+        fig, ax = plt.subplots(figsize=(3, 4), dpi=200)
+        ax.barh(df[x], df[y], color=rgba_list)
+        plt.xlabel(y)
+        plt.title(fig_title)
+        plt.tight_layout()
+        plt.show()
+
+
+def caculate_correlation(df: pd.DataFrame, target: str,
+                         verbose: bool = False) -> pd.DataFrame:
+    """
+    給定一個dataframe，計算所有欄位對於目標變數的相關係數
+
+    Args:
+        df (pd.DataFrame): 需要計算相關係數的dataframe
+        target (str): 目標變數
+        verbose(bool) : 是否需要顯示各個變數對目標變數的相關係數 Default to False
+
+    Returns:
+        df (pd.DataFrame): 計算完成的dataframe，內含兩個欄位 : 'features', 'corr_with_targets'
+
+    """
     import operator
 
     individual_features_df = []
-    num_cols = [var for var in df.columns if df[var].dtypes != 'O']
-    df_num = df[num_cols]
-    for i in range(0, len(df_num.columns) - 1):
-        tmp_df = df_num[[df_num.columns[i], target]]
-        tmp_df = tmp_df[tmp_df[df_num.columns[i]] != 0]
+    n_features = len(df.columns) - 1
+    for i in range(0, n_features):
+        tmp_df = df[[df.columns[i], target]]
+        tmp_df = tmp_df[tmp_df[df.columns[i]] != 0]
         individual_features_df.append(tmp_df)
 
     all_correlations = {feature.columns[0]: feature.corr(
@@ -73,24 +121,15 @@ def plot_feature_corr_sorting(df: pd.DataFrame, target: str,
     if verbose:
         for (key, value) in all_correlations:
             print("{:>15}: {:>15}".format(key, value))
-    df_corr = pd.DataFrame(all_correlations, columns=[
-                           'features', 'corr_with_targets'])
 
-    rgba_list, _ = colormap2rgb(num_colors=df_corr.shape[0])
-    with plt.style.context(['science', 'grid', 'no-latex']):
-        fig, ax = plt.subplots(figsize=(3, 4), dpi=200)
-        ax.barh(df_corr['features'],
-                df_corr['corr_with_targets'],
-                color=rgba_list)
-        plt.xlabel('features')
-        plt.title(fig_title)
-        plt.tight_layout()
-        plt.show()
+    return pd.DataFrame(all_correlations, columns=['features', 'corr_with_targets'])
 
 
 def main():
     df = pd.read_csv('data/house_price.csv')
-    plot_feature_corr_sorting(df=df, target='SalePrice')
+    df_num = filter_col(df)
+    df_corr = caculate_correlation(df_num, target='SalePrice')
+    plot_barh(df_corr)
 
 
 if __name__ == "__main__":
